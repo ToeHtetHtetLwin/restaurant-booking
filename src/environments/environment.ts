41b0ed06e684cf1df928251b14fb4a3dda8d0a5f
Htet <toehtethtetlwin123456@gmail.com>
2026-01-24 20:10:38 +0630

    fix correct url path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://695e07292556fd22f67712d6.mockapi.io/api/v1/menus',
+  apiUrl: 'https://695e07292556fd22f67712d6.mockapi.io/api/v1',
 };
